aaf4c2d0743a3140bc90f7f4750dadb82ac50242
Muhammad Sohaib Kamran <114664734+Sohaibkamran-25@users.noreply.github.com>
2026-06-07 01:55:34 +0500

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import app from './app.js';
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`GMAA backend running on port ${PORT}`);
-});
